884b2aba9968d8c63cf4e4e4b2d8fc9f1237001e
powerfeatherdev <dev@powerfeather.dev>
2023-08-26 10:34:04 +0900

    Implement iperf

--- test/test_board.cpp
+++ test/test_board.cpp
@@ -3,8 +3,11 @@
 #include <esp_private/system_internal.h>
 #include <esp_sleep.h>
 #include <driver/ledc.h>
+#include <esp_wifi.h>
+#include <nvs_flash.h>
 
-#include "unity.h"
+#include <unity.h>
+#include <iperf.h>
 
 #include <Board.h>
 #include <BQ2562x.h>
@@ -324,12 +327,104 @@ TEST_CASE("discharging and charging", MODULE_NAME)
     }
 }
 
+static void wifi_event_handler(void* arg, esp_event_base_t event_base,
+                                    int32_t event_id, void* event_data)
+{
+    if (event_id == WIFI_EVENT_AP_STACONNECTED) {
+        wifi_event_ap_staconnected_t* event = (wifi_event_ap_staconnected_t*) event_data;
+        printf("station "MACSTR" join, AID=%d\n", MAC2STR(event->mac), event->aid);
+    } else if (event_id == WIFI_EVENT_AP_STADISCONNECTED) {
+        wifi_event_ap_stadisconnected_t* event = (wifi_event_ap_stadisconnected_t*) event_data;
+        printf("station "MACSTR" leave, AID=%d\n", MAC2STR(event->mac), event->aid);
+    }
+}
+
+static esp_netif_t *wifi_netif = NULL;
+
+static void iperf_server()
+{
+    // Start iperf
+    iperf_cfg_t cfg;
+    memset(&cfg, 0, sizeof(cfg));
+
+    esp_netif_ip_info_t ip;
+    esp_netif_get_ip_info(wifi_netif, &ip);
+
+    cfg.flag |= IPERF_FLAG_SERVER | IPERF_FLAG_TCP;
+    // cfg.destination_ip4;
+    cfg.source_ip4 = ip.ip.addr;
+    cfg.type = IPERF_IP_TYPE_IPV4;
+    cfg.dport = IPERF_DEFAULT_PORT;
+    cfg.sport = IPERF_DEFAULT_PORT;
+    cfg.interval = IPERF_DEFAULT_INTERVAL;
+    cfg.time = IPERF_DEFAULT_TIME;
+    cfg.len_send_buf = 0;
+    cfg.bw_lim = IPERF_DEFAULT_NO_BW_LIMIT;
+
+    iperf_start(&cfg);
+}
+
+static void start_ap()
+{
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        nvs_flash_erase();
+        ret = nvs_flash_init();
+    }
+
+    esp_netif_init();
+    esp_event_loop_create_default();
+    wifi_netif = esp_netif_create_default_wifi_ap();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    esp_wifi_init(&cfg);
+
+    esp_event_handler_instance_register(WIFI_EVENT,
+                                        ESP_EVENT_ANY_ID,
+                                        &wifi_event_handler,
+                                        NULL,
+                                        NULL);
+
+    wifi_config_t wifi_config;
+
+    static constexpr char SSID[] = "powerfeather-ap";
+    static constexpr char PASS[] = "powerfeather-pswd";
+
+    memset(&wifi_config, 0, sizeof(wifi_config));
+
+    strncpy(reinterpret_cast<char*>(wifi_config.ap.ssid), SSID, sizeof(wifi_config.ap.ssid));
+    strncpy(reinterpret_cast<char*>(wifi_config.ap.password), PASS, sizeof(wifi_config.ap.password));
+
+    wifi_config.ap.ssid_len = strlen(SSID);
+    wifi_config.ap.channel = 6;
+    wifi_config.ap.max_connection = 4;
+    wifi_config.ap.authmode = WIFI_AUTH_WPA_WPA2_PSK;
+    if (strlen(PASS) == 0) {
+        wifi_config.ap.authmode = WIFI_AUTH_OPEN;
+    }
+
+    esp_wifi_set_mode(WIFI_MODE_AP);
+    esp_wifi_set_config(WIFI_IF_AP, &wifi_config);
+    esp_wifi_start();
+}
+
 TEST_CASE("current loading", MODULE_NAME)
 {
-    // Perform iperf test
+    // Run iperf server
     // 5V is loaded up to 2.5A
     // 3.3V is loaded up to 500mA
     // Measure ibus current
+    board.getCharger().enableADC(true, BQ2562x::ADCRate::Continuous);
+
+    start_ap();
+    // iperf_server();
+
+    while (true)
+    {
+        float ibus = board.getCharger().getIBUS();
+        printf("ibus: %.2f\n", ibus);
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
 }
 
 void wait_for_battery(uint32_t delay_ms)
