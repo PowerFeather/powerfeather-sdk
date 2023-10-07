aa21ab07241330ddfc4f3f547cf2bc4fd5051723
powerfeatherdev <dev@powerfeather.dev>
2023-10-07 14:20:44 +0900

    Initialize GPIO properly for LED test

--- test/main/test/test_board.cpp
+++ test/main/test/test_board.cpp
@@ -197,6 +197,29 @@ TEST_CASE("button and led", MODULE_NAME)
 {
     // Tie potentiometer to temperature sense
     // Check interrupt, may be combined with another test
+    //zero-initialize the config structure.
+    gpio_config_t io_conf = {};
+    //disable interrupt
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    //set as output mode
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    //bit mask of the pins that you want to set,e.g.GPIO18/19
+    io_conf.pin_bit_mask = (uint64_t)0b1 << PowerFeather::MainBoard::Pin::FF::LED;
+    //disable pull-down mode
+    io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    //disable pull-up mode
+    io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
+    //configure GPIO with the given settings
+    gpio_config(&io_conf);
+
+    //interrupt of rising edge
+    //bit mask of the pins, use GPIO4/5 here
+    io_conf.pin_bit_mask = (uint64_t)0b1 << PowerFeather::MainBoard::Pin::FF::BTN;
+    //set as input mode
+    io_conf.mode = GPIO_MODE_INPUT;
+    //enable pull-up mode
+    io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
+    gpio_config(&io_conf);
     gpio_set_level(MainBoard::Pin::FF::LED, true);
 
     gpio_set_intr_type(MainBoard::Pin::FF::BTN, GPIO_INTR_ANYEDGE);
