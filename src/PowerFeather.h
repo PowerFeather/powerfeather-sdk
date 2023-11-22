1ba3e7988832ce8369232aa22938a4216301b894
powerfeatherdev <dev@powerfeather.dev>
2023-11-22 15:07:00 +0800

    Add license text to source/header files

--- src/PowerFeather.h
+++ src/PowerFeather.h
@@ -32,24 +32,4 @@
  *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-#include "Mutex.h"
-
-namespace PowerFeather
-{
-
-    void Mutex::init()
-    {
-        _sem = xSemaphoreCreateRecursiveMutex();
-    }
-
-    bool Mutex::lock()
-    {
-        return xSemaphoreTakeRecursive(_sem, pdMS_TO_TICKS(_timeout));
-    }
-
-    void Mutex::unlock()
-    {
-        xSemaphoreGiveRecursive(_sem);
-    }
-}
-
+#include "MainBoard.h"
