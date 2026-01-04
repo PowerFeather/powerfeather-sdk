eee895c937b0b639f3c35ac752d9fc12c5082c43
powerfeatherdev <dev@powerfeather.dev>
2026-01-04 15:08:52 +0900

    Initial removal of hardcoded max17260 values

--- src/Mainboard/MAX17260.h
+++ src/Mainboard/MAX17260.h
@@ -55,27 +55,67 @@ namespace PowerFeather
         static constexpr float MaxTermination = 1.0f;
 
         static constexpr uint8_t _i2cAddress = 0x6c;
+        static constexpr uint8_t VAlrtTh_Register = 0x01;
+        static constexpr uint8_t SAlrtTh_Register = 0x03;
+        static constexpr uint8_t RepCap_Register = 0x05;
+        static constexpr uint8_t RepSOC_Register = 0x06;
+        static constexpr uint8_t Temp_Register = 0x08;
+        static constexpr uint8_t VCell_Register = 0x09;
+        static constexpr uint8_t FullCap_Register = 0x0A;
+        static constexpr uint8_t Register_0B = 0x0B;
+        static constexpr uint8_t MixCap_Register = 0x0F;
+        static constexpr uint8_t FullCapRep_Register = 0x10;
+        static constexpr uint8_t TTE_Register = 0x11; // 0x11 is TTE
+        static constexpr uint8_t QRTable00_Register = 0x12;
+        static constexpr uint8_t FullSOCThr_Register = 0x13;
+        static constexpr uint8_t Cycles_Register = 0x17;
+        static constexpr uint8_t DesignCap_Register = 0x18;
         static constexpr uint8_t Config_Register = 0x1D;
+        static constexpr uint8_t IChgTerm_Register = 0x1E;
+        static constexpr uint8_t AvCap_Register = 0x1F;
+        static constexpr uint8_t TTF_Register = 0x20;
+        static constexpr uint8_t DevName_Register = 0x21;
+        static constexpr uint8_t QRTable10_Register = 0x22;
+        static constexpr uint8_t FullCapNom_Register = 0x23;
+        static constexpr uint8_t LearnCfg_Register = 0x28;
+        static constexpr uint8_t RelaxCfg_Register = 0x2A;
+        static constexpr uint8_t MiscCfg_Register = 0x2B;
+        static constexpr uint8_t TGain_Register = 0x2C;
+        static constexpr uint8_t TOff_Register = 0x2D;
+        static constexpr uint8_t QRTable20_Register = 0x32;
+        static constexpr uint8_t RComp0_Register = 0x38;
+        static constexpr uint8_t TempCo_Register = 0x39;
+        static constexpr uint8_t VEmpty_Register = 0x3A;
+        static constexpr uint8_t QRTable30_Register = 0x42;
+        static constexpr uint8_t dQAcc_Register = 0x45;
+        static constexpr uint8_t dPAcc_Register = 0x46;
+        static constexpr uint8_t Command_Register = 0x60;
+        static constexpr uint8_t UnlockModel1_Register = 0x62;
+        static constexpr uint8_t UnlockModel2_Register = 0x63;
+        static constexpr uint8_t ModelTableStart_Register = 0x80;
+        static constexpr uint8_t RCompSeg_Register = 0xAF;
+        static constexpr uint8_t Curve_Register = 0xB9;
+        static constexpr uint8_t HibCfg_Register = 0xBA;
+        static constexpr uint8_t Config2_Register = 0xBB;
+        static constexpr uint8_t ModelCfg_Register = 0xDB;
+
+        static constexpr uint16_t DevName_MAX17260 = 0x4031;
+        static constexpr uint16_t UnlockKey1 = 0x0059;
+        static constexpr uint16_t UnlockKey2 = 0x00C4;
+        static constexpr uint16_t HibernateExit_Command1 = 0x0090;
+        static constexpr uint16_t HibernateExit_Command2 = 0x0000;
+
+        static constexpr uint8_t FullCapRep_Register = 0x10;
+
         static constexpr uint16_t ConfigBit_TSel = 1u << 15;
         static constexpr uint16_t ConfigBit_TEn = 1u << 9;
         static constexpr uint16_t ConfigBit_TEx = 1u << 8;
         static constexpr uint16_t ConfigBit_ETHRM = 1u << 4;
         static constexpr uint16_t ConfigBit_FTHRM = 1u << 3;
 
-        static constexpr uint8_t VAlrtTh_Register = 0x01;
-        static constexpr uint8_t SAlrtTh_Register = 0x03;
-        static constexpr uint8_t RepSOC_Register = 0x06;
-        static constexpr uint8_t Temp_Register = 0x08;
-        static constexpr uint8_t VCell_Register = 0x09;
-        static constexpr uint8_t FullCapRep_Register = 0x10;
-        static constexpr uint8_t TTE_Register = 0x11;
-        static constexpr uint8_t Cycles_Register = 0x17;
-        static constexpr uint8_t DesignCap_Register = 0x18;
-        static constexpr uint8_t IChgTerm_Register = 0x1E;
-        static constexpr uint8_t TTF_Register = 0x20;
-        static constexpr uint8_t ModelCfg_Register = 0xDB;
-
-        const Field Status_POR =            { 0x00, 1, 1 };
+    public:
+        static constexpr uint8_t ModelID_LiCoO2 = 2;
+        static constexpr uint8_t ModelID_LFP = 6;
         const Field FStat_DNR =             { 0x3D, 0, 0 };
         const Field Status_Vmn =            { 0x00, 8, 8 };
         const Field Status_Vmx =            { 0x00, 12, 12 };
